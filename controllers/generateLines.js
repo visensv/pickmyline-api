40b6ed8b2f117101b239b3d9fe8584f41f71a15d
Vibhooti Visen <vibhooti.visen@paytm.com>
2023-02-05 21:54:49 +0530

    removed node tls check

--- controllers/generateLines.js
+++ controllers/generateLines.js
@@ -1,5 +1,5 @@
 const { Configuration, OpenAIApi } = require('openai');
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
+//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
